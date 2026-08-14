0d73f75399a52bb591ca72342e28e6d2ac9e8c60
Juanchiz1 <juandinegrete2006@outlook.com>
2026-08-14 12:20:45 -0500

    Día 55 completado

--- dia54/helloFlask/hello.py
+++ dia54/helloFlask/hello.py
@@ -3,11 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "<h1>Hello, World!</h1>"\
+        "<p>Welcome to my Flask app!</p>"\
+        "<img src='https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif' alt='Hello GIF'>"    
 
 @app.route('/bye')
 def say_bye():
-    return 'Goodbye, World!'
+    return '<h1>Goodbye, World!</h1>'
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f'<h1>Hello there, {name}!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
